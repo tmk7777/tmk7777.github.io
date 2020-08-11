b7fee9f7cae6d552bb91af2b7d285ddde8b27a96
tmk7777 <tawheedcse63@gmail.com>
2020-08-11 22:23:35 +0600

    made images unclickable

--- public/javascript/main-js.js
+++ public/javascript/main-js.js
@@ -285,5 +285,15 @@
                 'slow');
         });
 
+        // this will disable dragging of all images
+        $("img").mousedown(function (e) {
+            e.preventDefault()
+        });
+
+        // this will disable right-click on all images
+        $("img").on("contextmenu", function (e) {
+            return false;
+        });
+
 
     };
